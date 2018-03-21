2a2a6a77670a3d20cfdd7a29e6fdc7cb4d36a230
warhammercasey <warhammercasey@gmail.com>
2018-03-20 22:00:39 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,30 +7,9 @@ client.on('ready', () => {
 	console.log('I am ready!');
 });
 
-client.on('message', message => {
-	message.reply('it got the thing, sendmessage is a little bitch.');
-	client.sendMessage({
-				to: channelID,
-				message: "Its getting the command, message.substring does not work for some bullshit reason"
-			});
-	if (message.substring(0, 1) == '!') {
-		var command = message.substring(1).split(' ');
-		var cmd = command[0];
-		command = command.splice(1);
-		client.sendMessage({
-				to: channelID,
-				message: "At least this part is working"
-			});
-		if(cmd == "lastMessage") {
-			client.sendMessage({
-				to: channelID,
-				message: "Your dumbass needs quotes or something"
-			});
-			client.sendMessage({
-				to: channelID,
-				message: message.mentions.users.first() + " last logged in at "
-			});
-		}
+clinet.on('message', message=> {
+	if (message.content == 'ping') {
+		message.reply('pong');
 	}
 });
 
