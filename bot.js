e2addef959c9cf652747482ba3d73b6b4bdfb4b6
warhammercasey <warhammercasey@gmail.com>
2018-03-20 21:35:15 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,24 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+var names = [];
+var lastMessageTime = [];
+
 client.on('ready', () => {
 	console.log('I am ready!');
 });
 
-client.on('message', message => {
-	if (message.content === 'will you be my friend discord bot?') {
-		message.reply("No, stop fucking asking me");
+client.on('message', function (channelID, message) {
+	if (message.substring(0, 1) == '!') {
+		var command = message.substring(1).split(' ');
+		var cmd = command[0];
+		command = command.splice(1);
+		if(cmd == lastMessage) {
+			bot.sendMessage({
+				to: channelID,
+				message: message.mentions.users.first() + " last logged in at "
+			});
+		}
 	}
 });
 
