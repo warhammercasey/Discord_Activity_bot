23d30d8645344acbd51d0ad229131b6fc742877d
warhammercasey <warhammercasey@gmail.com>
2018-03-20 21:36:11 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,7 +13,7 @@ client.on('message', function (channelID, message) {
 		var command = message.substring(1).split(' ');
 		var cmd = command[0];
 		command = command.splice(1);
-		if(cmd == lastMessage) {
+		if(cmd == "lastMessage") {
 			bot.sendMessage({
 				to: channelID,
 				message: message.mentions.users.first() + " last logged in at "
