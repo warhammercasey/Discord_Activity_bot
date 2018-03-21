5f8c5202c4ff0ede1492255184c939d0524d8b0e
warhammercasey <warhammercasey@gmail.com>
2018-03-20 20:27:40 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,39 +1,13 @@
-var Discord = require('discord.io');
-var logger = require('winston');
-var auth = require('./auth.json');
-//configure logger settings
-logger.remove(logger.transports.Console);
-logger.add(logger.transports.Console, {
-	colorize: true
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+	console.log('I am ready!');
 });
-logger.level = 'debug';
-//Initalize Discord bot
-var bot = new Discord.Client({
-	token: auth.token,
-	autorun: true
-});
-bot.on('ready', function (evt) {
-	logger.info('Connected');
-	logger.info('Logged in as: ');
-	logger.info(bot.username + ' - (' + bot.id + ')');
-});
-bot.on('message', function (user, userID, channelID, message, evt) {
-	//Bot needs to know if it will execute a command
-	//It will listen for messages that start with !
-	if(message.substring(0, 1) == '!') {
-		var args = message.substring(1).split(' ');
-		var cmd = args[0];
-		
-		args = args.splice(1);
-		switch(cmd) {
-			// !ping
-			case 'ping':
-				bot.sendMessage({
-					to: channelID,
-					message: 'Pong!'
-				});
-			break;
-		}
+
+client.on('message', message => {
+	if (message.content === 'ping') {
+		message.reply('pong');
 	}
 });
 
