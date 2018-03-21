d737545eb5e48f3fe9969e54ca00bea291626917
warhammercasey <warhammercasey@gmail.com>
2018-03-21 16:50:31 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ client.on('message', message => {
 	console.log(message.content.charAt(0));
 	if(message.content.charAt(0) == '!') {
 		console.log("Recognised '!'.");
-		if(message.content.prototype.subString(1, 8) == "lastLog"){
+		if(message.content.substr(0, message.content.indexOf(" ")) == "lastLog"){
 			message.reply("Got message, time is: ");
 			message.reply(message.mentions.users.first().lastMessage.createdAt);
 		}
