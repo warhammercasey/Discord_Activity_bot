80ef0b999397edd17d3ed6af4c0ed3ff670e2c4d
warhammercasey <warhammercasey@gmail.com>
2018-03-22 16:52:54 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -46,13 +46,8 @@ client.on('message', message => {
 	}
 });
 
-var fs = require('fs');
-fs.writeFile("/tmp/test", "Hey there!", function(err) {
-    if(err) {
-        return console.log(err);
-    }
-
-    console.log("The file was saved!");
-}); 
+client.on('guildMemberAdd', guildMember => {
+	guildMembers.push(guildMember);
+});
 
 client.login(process.env.BOT_TOKEN);
