c6a779822a0b3e6b4d79bcfc9780f013150c49c1
warhammercasey <warhammercasey@gmail.com>
2018-03-29 00:32:55 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,7 +17,6 @@ client.on('message', message => {
 	console.log(message.createdAt.toString().concat("|", "text", "|", message.member.user.id.toString(), "|", message.member.user.username.toString(), "|", message.channel.id.toString(), "|", message.channel.name.toString(), "|", message.content.length));
 	fs.readFile('data.txt', (err, data) => {
   		if (err) throw err;
-  		console.log("Data: " + data);
 		fileData = data;
 	});
 	if(fileData == undefined){
@@ -25,7 +24,6 @@ client.on('message', message => {
 			if(err) {
 				return console.log(err);
 			}
-			console.log("The file was saved!");
 		});
 		return;
 	}
@@ -33,7 +31,6 @@ client.on('message', message => {
 		if(err) {
 			return console.log(err);
 		}
-		console.log("The file was saved!");
 	}); 
 	
 });
@@ -44,7 +41,6 @@ client.on('voiceStateUpdate', (oldMember, newMember) => {
 		console.log(currentTime.toString().concat("|", "VoiceJoin", "|", newMember.user.id.toString(), "|", newMember.user.username.toString(), "|", newMember.voiceChannel.id.toString(), "|", newMember.voiceChannel.name.toString()));
 		fs.readFile('data.txt', (err, data) => {
   			if (err) throw err;
-  			console.log(data);
 			fileData = data;
 		});
 		if(fileData == undefined){
@@ -52,20 +48,17 @@ client.on('voiceStateUpdate', (oldMember, newMember) => {
 			if(err) {
 				return console.log(err);
 			}
-			console.log("The file was saved!");
 		}); 
 		}
 		fs.writeFile("data.txt", fileData.toString().concat(" - ", currentTime.toString(), "|", "VoiceJoin", "|", newMember.user.id.toString(), "|", newMember.user.username.toString(), "|", newMember.voiceChannel.id.toString(), "|", newMember.voiceChannel.name.toString()), function(err) {
 			if(err) {
 				return console.log(err);
 			}
-			console.log("The file was saved!");
 		}); 
 	}else if(oldMember.voiceChannel != null && newMember.voiceChannel == null){
 		console.log(currentTime.toString().concat("|", "VoiceLeave", "|", oldMember.user.id.toString(), "|", oldMember.user.username.toString(), "|", oldMember.voiceChannel.id.toString(), "|", oldMember.voiceChannel.name.toString()));
 		fs.readFile('data.txt', (err, data) => {
   			if (err) throw err;
-  			console.log(data);
 			fileData = data;
 		});
 		if(fileData == undefined){
@@ -73,14 +66,12 @@ client.on('voiceStateUpdate', (oldMember, newMember) => {
 				if(err) {
 					return console.log(err);
 				}
-				console.log("The file was saved!");
 			}); 
 		}
 		fs.writeFile("data.txt", fileData.toString().concat(" - ", currentTime.toString(), "|", "VoiceLeave", "|", oldMember.user.id.toString(), "|", oldMember.user.username.toString(), "|", oldMember.voiceChannel.id.toString(), "|", oldMember.voiceChannel.name.toString()), function(err) {
 			if(err) {
 				return console.log(err);
 			}
-			console.log("The file was saved!");
 		}); 
 	}
 });
