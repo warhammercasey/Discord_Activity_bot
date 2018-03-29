9829ede9181ec3cd01626d2710bffb3ad602b1bc
warhammercasey <warhammercasey@gmail.com>
2018-03-28 23:39:26 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,71 +2,18 @@ const Discord = require('discord.js');
 const fs = require('fs');
 const client = new Discord.Client();
 
-var createdDate = new Date();
-var hasFirstDate = false;
-var assignedChannel;
-
-var guildMembers = [];
-var guildUsers = [];
-var userMessageTimes = [];
-var messageTimeStamps = [];
-
-var voiceChannelJoin = [];
-var voiceChannelLeave = [];
-
-var canCheckMessages = false;
-
-var logStrings = [];
-
-client.on('ready', () => {
-	console.log('I am ready!');
-});
+var disabled = false;
 
 client.on('message', message => {
-	if(!hasFirstDate){
-		createdDate = message.createdAt;
-		hasFirstDate = true;
-		guildMembers = [message.guild.memberCount];
- 		//console.log(message.guild.members);
-		guildMembers = message.guild.members.array();
-		for(i = 0; i < message.guild.memberCount; i++){
-			guildUsers.push(guildMembers[i].user);
+	if(message.content.charAt(0) == '!') { //Is command
+		if(message.content.substr(1, message.content.indexOf(" ") - 1) == "loginBotDisable"){
+			disabled = true;
 		}
-		totalMessages = [guildUsers.length];
-		voiceChannelJoin = [guildUsers.length];
-		totalVCTime = [guildUsers.length];
+		if(message.content.substr(1, message.content.indexOf(" ") - 1) == "loginBotEnable"){
+			disabled = false;
+		}
 	}
-	if(message.mentions.users.first() == client.user){
-		canCheckMessages = false;
-		assignedChannel = message.channel;
-		assignedChannel.send('I have now been assigned to this channel.')
-			.then(message => console.log(`Sent start message: ${message.content}`))
-			.catch(console.error);
-		assignedChannel.send('To change the assigned channel just mention me somewhere else.')
-			.then(message => console.log(`Sent start message: ${message.content}`))
-			.catch(console.error);
-		assignedChannel.send('Commands (begin all with !):')
-			.then(message => console.log(`Sent start message: ${message.content}`))
-			.catch(console.error);
-		assignedChannel.send('lastMsg <mention user> - Displays the last time that user sent a message.')
-			.then(message => console.log(`Sent start message: ${message.content}`))
-			.catch(console.error);
-		assignedChannel.send('lastVC <mention user> - Displays the last time that user joined a voice channel.')
-			.then(message => console.log(`Sent start message: ${message.content}`))
-			.catch(console.error);
-		assignedChannel.send('totalMessages <mention user> - Displays how many messages that user has sent.')
-			.then(message => console.log(`Sent start message: ${message.content}`))
-			.catch(console.error);
-		assignedChannel.send('VCTime <mention user> - Displays how much time that user has spent in any voice chat.')
-			.then(message => console.log(`Sent start message: ${message.content}`))
-			.catch(console.error);
-		assignedChannel.send('Remember that until a database is set up, the data collected is temporary, and will be reset if the bot is restarted or updated.')
-			.then(message => console.log(`Sent start message: ${message.content}`))
-			.catch(console.error);
-		canCheckMessages = true;
-	}
-	if(!canCheckMessages){return;}
-	
+	if(!disabled){return;}
 	console.log(message.createdAt.toString().concat("|", "text", "|", message.member.user.id.toString(), "|", message.member.user.username.toString(), "|", message.channel.id.toString(), "|", message.channel.name.toString(), "|", message.content.length));
 	fs.writeFile("data.txt", message.createdAt.toString().concat("|", "text", "|", message.member.user.id.toString(), "|", message.member.user.username.toString(), "|", message.channel.id.toString(), "|", message.channel.name.toString(), "|", message.content.length), function(err) {
 			if(err) {
@@ -74,20 +21,11 @@ client.on('message', message => {
 			}
 			console.log("The file was saved!");
 		}); 
-	if(message.content.charAt(0) == '!') { //Is command
-		
-	}
-	console.log("Channel: " + assignedChannel);
-	console.log(message.author.username + " has sent " + totalMessages[guildUsers.indexOf(message.author)] + " in this server.");
-});
-
-client.on('guildMemberAdd', member => {
-	guildUsers.push(member.user);
-	totalMessages = [guildUsers.length];
-	console.log("Added user: " + member.user);
+	
 });
 
 client.on('voiceStateUpdate', (oldMember, newMember) => {
+	if(!disabled){return;}
 	var currentTime = new Date();
 	if(oldMember.voiceChannel == null && newMember.voiceChannel != null){
 		console.log(currentTime.toString().concat("|", "VoiceJoin", "|", newMember.user.id.toString(), "|", newMember.user.username.toString(), "|", newMember.voiceChannel.id.toString(), "|", newMember.voiceChannel.name.toString()));
