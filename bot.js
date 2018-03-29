711c8b56c00ef5a1c240dbe297c2433727687650
warhammercasey <warhammercasey@gmail.com>
2018-03-29 17:35:02 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -31,7 +31,7 @@ client.on('message', message => {
 		});
 		return;
 	}
-	console.log(fileData.toString());
+	//console.log(fileData.toString());
 	fs.writeFile("data.txt", fileData.toString().concat(date.toString(), "|", "text", "|", message.member.user.id.toString(), "|", message.member.user.username.toString(), "|", message.channel.id.toString(), "|", message.channel.name.toString(), "|", message.content.length) + "\r\n", function(err) {
 		if(err) {
 			return console.log(err);
@@ -42,6 +42,7 @@ client.on('message', message => {
 
 client.on('voiceStateUpdate', (oldMember, newMember) => {
 	var currentTime = new Date();
+	currentTime = (currentTime.getMonth() + 1).toString().concat("/", currentTime.getDate().toString(), "/", currentTime.getYear().toString().substr(1, 3), " ", currentTime.getUTCHours().toString(), ":", currentTime.getMinutes().toString(), ":", currentTime.getSeconds().toString());
 	if(oldMember.voiceChannel == null && newMember.voiceChannel != null){
 		console.log(currentTime.toString().concat("|", "VoiceJoin", "|", newMember.user.id.toString(), "|", newMember.user.username.toString(), "|", newMember.voiceChannel.id.toString(), "|", newMember.voiceChannel.name.toString()));
 		fs.readFile('data.txt', (err, data) => {
